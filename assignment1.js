7e580076def0402ee890c0e66db787f23bad18cc
Arpit Soni <arpitsoni77757@gmail.com>
2025-06-08 21:20:28 +0530

    assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -1,1 +1,13 @@
 console.log("Hello World")
+
+    // --------- Key points which i have learned in Assignment 1 ---------
+
+/* 1. Press node -v in terminal to check the version of Node.js installed. if it is not showing then it is not installed yet.
+   2. Executing JavaScript with Node: Two ways  (i). Node REPL (Read-Eval-Print Loop) (ii). Node.js script file
+                  (i) Node.js REPL: It is an interactive shell that allows you to execute JavaScript code in real-time.
+                      In terminal, type `node` and press Enter. You can then type JavaScript code directly into the terminal. 
+                      in terminal then type our first Node.js program: console.log("Hello World!");
+                      It will print "Hello World!" to the console. 
+
+                  (ii) Simply type in terminal node filename.js to execute the JavaScript code in that file. it will print "Hello World!" to the console.
+*/
